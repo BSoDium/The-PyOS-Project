3ff5c23a4c495ebfba2ee59dfa06ce430e761c3c
Zudo <48180274+Zudokakikuto@users.noreply.github.com>
2019-03-23 23:55:40 +0100

    Added average eccentric
    Average accentric use average anomaly.
    Allows us to know the angle between the periapsis and the actual position in the orbit of the studied object.

--- Orbital analysis.py
+++ Orbital analysis.py
@@ -3,6 +3,7 @@ from panda3d.core import *
 from direct.gui.DirectGui import *
 import sys
 from math import *
+import numpy as np
 
 ### Orbital Analysis
 
@@ -15,12 +16,21 @@ from math import *
     To define our orbits we will need some parameters as : Eccentricity, Half-major axis, tilt, longitude of the ascending node, perry argument, periapsis and the position.
 
     About the position : 
-    Let's consider the average anomaly, which allows us to know the angle between the periapsis and the actual position of the planet in the orbit.
-    We note the average anomaly M, we have : 'G' the gravitationnal constant, 'a' the half major axis, 'Ms' the mass of the planet, 'm' the mass of the studied object.
+    Let's consider the average anomaly, which allows us to know the fraction of the orbital period that has elapsed since the last pass to the periapsis, expressed as an angle.
+    We note the average anomaly 'M', we have : 'G' the gravitationnal constant, 'a' the half major axis, 'Ms' the mass of the planet, 'm' the mass of the studied object.
     M = sqrt(G(Ms+m)/a³)*t
 '''
 
 def average_anomaly(Ms,m,t,a):
     M=sqrt(6.67408*10**-11*(Ms+m)/a**3)*t
     return M
+
+''' The average eccentric is the angle between the periapsis and the actual position IN the orbit of the studied object. Let's not the average eccentric 'E'. Then we have :
+The itération : E[i+1]=(M-a*(E[i]cos(E[i]-sin(E[i]))/1-a*cos(E[i])) , by doing 4 iteration, we obtain a precise value of E for a t time'''
+
+def average_eccentric(M,a):
+    E=np.pi
+    for i in range(1,5):
+        E=(M-a*(E*cos(E)-sin(E)))/1-a*cos(E)
+    return E
 
