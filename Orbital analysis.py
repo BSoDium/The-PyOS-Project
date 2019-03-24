922532b9e34f000661f1ea3a7a76010a4df5238a
Zudo <48180274+Zudokakikuto@users.noreply.github.com>
2019-03-24 09:00:36 +0100

    Fixed bugs

--- Orbital analysis.py
+++ Orbital analysis.py
@@ -11,7 +11,7 @@ import numpy as np
 
 ''' What's an orbit ? 
     It's the gravitationally curved trajectory of an object 
-    The major part of orbits are supposed circulars, but to fit with reality let's consider them elliptics. (After we will add functions to make them parabolic and hyperbolic)
+    The major part of orbits are supposed circulars, but to fit with reality let's consider them elliptics. (After we will add functions to make them parabolic or hyperbolic)
     There is severals types of orbits : Suborbitals trajectories and Escape trajectories 
     To define our orbits we will need some parameters as : Eccentricity, Half-major axis, tilt, longitude of the ascending node, perry argument, periapsis and the position.
 
@@ -25,11 +25,12 @@ def average_anomaly(Ms,m,t,a):
     M=sqrt(6.67408*10**-11*(Ms+m)/a**3)*t
     return M
 
+
 ''' The average eccentric is the angle between the periapsis and the actual position projected in the cercle excircled perpendicularly to the half major axis. 
 Let's note the average eccentric 'E'. Then with 'e' the exxentricity , we have :
 The iteration : E[i+1]=(M-e*(E[i]cos(E[i]-sin(E[i]))/1-e*cos(E[i])) , by doing 4 iterations, we obtain a precise value of E for a t time'''
 
-def average_eccentric(M,a):
+def average_eccentric(M,e):
     E=np.pi
     for i in range(1,5):
         E=(M-e*(E*cos(E)-sin(E)))/1-e*cos(E)
@@ -40,7 +41,7 @@ tan(v/2) : sqrt(1+e/1-e)*tan(E/2)
 '''
 
 def real_anomaly(E,e):
-    v=2*np.arctan(sqrt(1+e/1-e))*tant(E/2)
+    v=2*np.arctan(sqrt(1+e/1-e))*np.tan(E/2)
     return v
 
 ''' If we want to know what is the eccentricity of an orbit, thanks to apoapsis radius(Ra) and periapsis radius(Rp), with :
