6842f12728b31465332d08c0b8e0565907f0b54e
Fang31 <48260790+Fang31@users.noreply.github.com>
2019-03-25 13:40:44 +0100

    This one actually works. I guess, perhaps. Do you want a cookie?
    Title is as long as description because it actually is the description.
    Changed stuff that doesn't work, if you really want to know....

--- Engine/Force.py
+++ Engine/Force.py
@@ -3,33 +3,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-#Les corps célestes sont des listes du type P=[Masse,Rayon/Taille,x,y,z]
-#Donc C est une liste de tous les corps à prendre en compte. C=[P1,P2...]
+#Celestial objects are lists created with this pattern: [Weigt,Length,x,y,z]
+#C is defined as a list of celestial bodies, constructed with the informations the user selected. C=[P1,P2...]
+
 
 def accélération(C):
-    G=6,673*10**-11
-    A=[]
-    O=[]
+    G=6.673*10**-11
+    O=[]  #This will be the list with the accelerations for an object
     n=len(C)
+    A=[0 for i in range(n)]  #The future list of all accelerations for all objects.
     for i in range(0,n):
         x=0
         y=0
         z=0
         for k in range(i+1,n):
-            d=sqrt((C[k][2]-C[i][2])*2+(C[k][3]-C[i][3])*2+(C[k][4]-C[i][4])*2)
-            x+=((C[k][0]*G*(C[i][2]-C[k][2])/sqrt((C[i][2]-C[k][2])*2+(C[i][3]-C[k][3])*2))/d**2)+C[k][2]
-            y+=((C[k][0]*G*np.sin(np.arccos((C[i][2]-C[k][2])/sqrt((C[i][2]-C[k][2])*2+(C[i][3]-C[k][3])*2))))/d**2)+C[k][3]
-            z+=C[k][4]
-        O[0]=x
-        O[1]=y
-        O[2]=z
+            d=sqrt((C[k][2]-C[i][2])**2+(C[k][3]-C[i][3])**2+(C[k][4]-C[i][4])**2)
+            x+=(G*C[i][0]*(C[k][2]-C[i][2]))/d**2
+            y+=(G*C[i][0]*(C[k][3]-C[i][3]))/d**2
+            z+=(G*C[i][0]*(C[k][4]-C[i][4]))/d**2
+        O.append(x)
+        O.append(y)
+        O.append(z)
     A[i]=O
     return A
-    
 
-
-
-            
-P1=[2*10**30,0,10**3,10**6,10**4]
-P2=[6*10**24,0,8*10**7,10**2,24*10**5] 
-C=[P1,P2]
