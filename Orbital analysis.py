5a67ffcd893b510222354806b7862a6bd398d840
Zudo <48180274+Zudokakikuto@users.noreply.github.com>
2019-04-03 13:51:05 +0200

    Final Release Orbital Analysis
    Remove useless librairies
    Debbuged functions
    Remove F function
    Added F's vector function
    Fix numpy functions
    Fix Orbital kinetic torque and find it with vectors
    Added tilt function
    Listing of functions

--- Orbital analysis.py
+++ Orbital analysis.py
@@ -9,12 +9,8 @@
 
 '''
 
-from direct.showbase.ShowBase import ShowBase
-from panda3d.core import *
-from direct.gui.DirectGui import *
-import sys
-from math import *
 import numpy as np
+import numpy.linalg as alg
 
 
 #       ___  _  _ _   _ ____ _ ____ ____ _       ____ ___  ___  ____ ____ ____ ____ _  _ 
@@ -24,55 +20,59 @@ import numpy as np
 
 
 ''' What's an orbit ? 
-    It's the gravitationally curved trajectory of an object 
-    The major part of orbits are supposed circulars, but to fit with reality let's consider them elliptics. (After we will add functions to make them parabolic or hyperbolic).
-    We take the ecuador of the created object as the plane reference.
-    There is severals types of orbits : Suborbitals trajectories and Escape trajectories.
-    To define our orbits we will need some parameters as : Eccentricity, Half-major axis, tilt, longitude of the ascending node, perry argument, periapsis and the position.
-'''
+It's the gravitationally curved trajectory of an object 
+The major part of orbits are supposed circulars, but to fit with reality let's consider them elliptics. (After we will add functions to make them 
+parabolic or hyperbolic).
+We take the ecuador of the created object as the plane reference.
+There is severals types of orbits : Suborbitals trajectories and Escape trajectories.
+To define our orbits we will need some parameters as : Eccentricity, Half-major axis, tilt, longitude of the ascending node, perry argument, 
+periapsis and the position.'''
 
 #    About the position : 
 
-''' Let's consider the mean anomaly, which allows us to know the fraction of the orbital period that has elapsed since the last pass to the periapsis, expressed as an angle.
-    We note the mean anomaly 'M', we have : 'G' the gravitationnal constant, 'a' the half major axis, 'Ms' the mass of the planet, 'm' the mass of the studied object.
-    M = sqrt(G(Ms+m)/a³)*t
-'''
+''' Let's consider the mean anomaly, which allows us to know the fraction of the orbital period that has elapsed since the last pass to the periapsis, 
+expressed as an angle.
+We note the mean anomaly 'M', we have : 'G' the gravitationnal constant, 'a' the half major axis, 'Ms' the mass of the planet, 'm' the mass of the 
+studied object.
+M = sqrt(G(Ms+m)/a³)*t'''
 
-def mean_anomaly(M,m,t,a):
-    M=sqrt(6.67408*10**-11*(Ms+m)/a**3)*t
+def mean_anomaly(Ms,m,t,a):
+    M=np.sqrt(6.67408*10**-11*(Ms+m)/a**3)*t
     return M
 
-''' The eccentric anomaly is the angle between the periapsis and the actual position projected in the cercle excircled perpendicularly to the half major axis. 
+
+''' The eccentric anomaly is the angle between the periapsis and the actual position projected in the cercle excircled perpendicularly to the half 
+major axis. 
 Let's note the eccentric anomaly 'E'. Then with 'e' the eccentricity , we have :
 The iteration : E[i+1]=(M-e*(E[i]cos(E[i]-sin(E[i]))/1-e*cos(E[i])) , by doing 4 iterations, we can obtain a precise value of E for a t time.'''
 
 def eccentric_anomaly(M,e):
     E=np.pi
     for i in range(1,5):
-        E=(M-e*(E*cos(E)-sin(E)))/1-e*cos(E)
+        E=(M-e*(E*np.cos(E)-np.sin(E)))/1-e*np.cos(E)
     return E
 
-''' Finally the true anomaly is the angle between the periapsis and the actual position of the studied object (that's what we want). We name the real anomaly 'v', then :
-tan(v/2) : sqrt(1+e/1-e)*tan(E/2)
-'''
+
+''' Finally the true anomaly is the angle between the periapsis and the actual position of the studied object (that's what we want). We name the real 
+anomaly 'v', then : tan(v/2) : sqrt(1+e/1-e)*tan(E/2)'''
 
 def true_anomaly(E,e):   # E = eccentric anomaly, e = eccentricity
-    v=2*np.arctan(sqrt(1+e/1-e))*np.tan(E/2)
+    v=2*np.arctan(np.sqrt(1+e/1-e))*np.tan(E/2)
     return v
 
 
 ''' If we want to know what is the eccentricity of an orbit, thanks to apoapsis radius(Ra) and periapsis radius(Rp), with :
 e = (Ra-Rp)/(Ra+Rp)
 or 
-e = sqrt(1-(b²/a²))   with b the half minor axis and a the half major axis.
-'''
+e = sqrt(1-(b²/a²))   with b the half minor axis and a the half major axis.'''
 
 def e(Ra,Rp):
     e = (Ra-Rp)/(Ra+Rp)
     return e
 
+
 def e_V2(a,b):
-    e=sqrt(1-(b**2/a**2))
+    e=np.sqrt(1-(b**2/a**2))
     return e
 
 
@@ -80,23 +80,26 @@ def e_V2(a,b):
 We have a r(E) relation, but also a E(t) relation so a r(E(t)) relation , a is the half major axis again'''
 
 def r(E,a,e):
-        r=a*(1-e*cos(E))
-        return r
+    r=a*(1-e*np.cos(E))
+    return r
+
 
 ''' We can now know what will be the orbital period and finally know what will be the 't' time where the studied object is at the periapsis : 
 by calculating the mean angular motion , named n'''
 
 def n(M,m,a):
-        n=sqrt(6.67408*10**-11*(m+M)/a**3)
-        return n
+    n=np.sqrt(6.67408*10**-11*(m+M)/a**3)
+    return n
+
 
 def T(n):
-        T=2*np.pi/n
-        return T     # Orbital period
+    T=2*np.pi/n
+    return T     # Orbital period
+
 
 def t_periapsis(M,n,t):
-        tperi=M/n-t
-        return tperi   # Time t0 where the object is at the periapsis
+    tperi=(M/n)-t
+    return tperi   # Time t0 where the object is at the periapsis
 
 
 ## What parameters will the user enter ?
@@ -119,8 +122,7 @@ So what the user will enter ?
 
 With theses value we can now know we will be the studied object in the orbit.
 
-Let's define now how to define this orbit with theses parameters.
-'''
+Let's define now how to define this orbit with theses parameters'''
 
 ##        __
 ##       /  \  _ |_  . |_ 
@@ -134,6 +136,7 @@ Let's consider the cylindrical coordinates :
         ->     ->                                                                              ->
 We call er and eθ respectively the elementary radial vector and the elementary tangent vector. ez is the elementary vector for height.
 For any M point we have in the ellipse :
+θ is the angle formed between the periapsis and the object.
 
 ->     ->     ->
 OM = r*er + z*ez
@@ -142,49 +145,61 @@ OM = r*er + z*ez
 v = (dr/dt)*er + r*(dθ/dt)*eθ + (dz/dt)*ez
 
 ->            ->                     ->                ->             ->
-a = (d²r/dt²)*er + 2*(dr/dt)*(dθ/dt)*eθ + r*(dθ/dt)**2*er + (d²z/dt²)*ez
-'''
+a = (d²r/dt²)*er + 2*(dr/dt)*(dθ/dt)*eθ + r*(dθ/dt)**2*er + (d²z/dt²)*ez '''
 
 ## Tilt :
 
 ''' To calculate the tilt, we need to find first the orbital kinetic torque. And to find this last, we need the force exerciced on the studied object.'''
 
-def F(M,m,r):  # Will be oriented in the contrary sens of the radial vector. So there is only a radial component.
+def F(M,m,v):  # Will be oriented in the contrary sens of the radial vector. So there is only a radial component, v is real anomaly and V is speed.
     F=6.67408*10**-11*((m*M)/r**2)
+    Fx=F*np.cos(v)
+    Fy=F*np.sin(v)
+    R=np.array([Fx,Fy,0])
+    return R
 
 
 ''' The orbital kinetic torque in a O point is :1
-    -------->   ->  ->      ->            ->             ->           ->                     ->               ->
-    L[orbital]= F ^ mv  = F*er m*((dr/dt)*er + r*(dθ/dt)*eθ + (dz/dt)*ez) ^  = F*m*r*(dθ/dt)*ez - F*m*(dz/dt)*eθ
+    -------->   ->  ->        ->            ->             ->           ->                      ->                ->
+    L[orbital]= OM ^ mv  = OM*er m*((dr/dt)*er + r*(dθ/dt)*eθ + (dz/dt)*ez) ^  = OM*m*r*(dθ/dt)*ez - OM*m*(dz/dt)*eθ
 
       ->             
-So on ez : L[orbital-z] = F*m*r*(dθ/dt)
+So on ez : L[orbital-z] = OM*m*r*(dθ/dt)
       -> 
-   on eθ : L[orbital-θ] = -F*m*(dz/dt)
+   on eθ : L[orbital-θ] = -OM*m*(dz/dt)
 
-'''
-
-'''Find the initial orbital kentic torque, it won't change, find the tilt by calculating the angle between the initial orbital kinetic torque and the ecuador plan. Then remove 90°
-and we will find what we wanted.'''
+Finding the initial orbital kentic torque is unique, it won't change, find the tilt by calculating the angle between the initial orbital kinetic 
+torque and the ecuador plan. To find cos(angle) this we will divide the e-component of the kinetic torque by the norm of this last, and then use arccos.'''
 
 # We will search the z-component of Lo(M) which is the orbital kinetic torque at a M point.   
 
-def inital_orbital_kinetic_torque_z(F,m,M,vo): # v0 is the inital speed, it will be entered by the user. 
-        #Lo(M)=F^m*v   with m the total mass
-        #    ->
-        # on ez : Lzo(M)=F*m*r*(dθ/dt) = F*m*r*Ω = F*m*v      # Where Ω is the angular speed , v=Ω*r
-        L=F*(m+M)*v0
-        return L
+def initial_orbital_kineic_torque(OM,m,v):  # OM is an array (so the vector) v is the vector with x,y,z components
+    r=m*v
+    L=np.dot(OM, r)
+    return L
+
 
 # Tilt : 
 
+'''Let's search the angle between the initial orbital torque and the ecuador plan (supposed to be the horizontally)'''
+
+def tilt(L):  # L is an array
+    Lz=L[2]     # The z component
+    e=Lz/L[0]+L[1]+L[2]
+    e=np.arccos(e)
+    return e
 
 
-''' Let's find now the argument of the periapsis :
+''' A little suumary of all the functions we have created:
 
-We will name ω the argument of the periapsis, which can be found thanks :
-ω = arccos((n*e)/|n|*|e|)
-Where n is a vector pointing towards the ascending node and e the eccentricity vector pointing towards the periapsis.
-'''
-
-
+- mean anomaly
+- eccentric anomaly
+- true anomaly
+- eccentricity
+- radius
+- mean angular motion
+- period
+- periapsis time
+- F
+- initial orbital kinetic torque
+- tilt'''
