7a278785121f236b30285ce7f736164e1803cc6e
Filip Jerzy <keltapolis@gmail.com>
2019-04-04 20:02:37 +0200

    finally got that f*cking fullscreen to work
    currently only supports 1600*900 monitors, but no prob, I will deal with portability issues later

--- Panda3d experiments/Skybox design test.py
+++ Panda3d experiments/Skybox design test.py
@@ -2,37 +2,62 @@ from math import *
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 from pandac.PandaModules import *
-from panda3d.core import WindowProperties
+from panda3d.core import *
 from direct.showbase import DirectObject # event handling
-import os
-
-#ConfigVariableBool('fullscreen').setValue(1)
-#wp=WindowProperties()
-#wp.setSize(1600,900)
-#from direct.directbase.DirectStart import *
+from direct.gui.OnscreenText import OnscreenText
+loadPrcFileData('', 'fullscreen true')
+loadPrcFileData('','win-size 1600 900') 
+#finalyyyyyyyyy got that f*cking fullscreen to woooork goddamit that was sooo f*cking hard to find
 
 class core(ShowBase):
 	def __init__(self):
 		ShowBase.__init__(self)
-		self.scene=self.loader.loadModel("random_rock.egg")
-		wp = WindowProperties()
-		wp.setFullscreen(1)
-		wp.setSize(1920, 1080)
-		Z=0
-		Y=0
+		self.ast=self.loader.loadModel("random_rock.egg")
+		self.sun=self.loader.loadModel("low_poly_planet.egg")
+		
 		base.openMainWindow()
-		base.win.requestProperties(wp)
 		base.graphicsEngine.openWindows()
-		self.scene.reparentTo(self.render)
-		self.scene.setScale(3,3,3)
-		self.scene.setPos(0,0,0)
-		self.objectspin(Z,Y)
+		self.ast.reparentTo(self.render)
+		self.ast.setScale(1,1,1)
+		self.bodyplace=(0,0,0) #positional variable
+		self.ast.setPos(0,0,0) 
+		self.step=0 #calculation variable, counts the number of frames passed
 
-	def objectspin(self,angle_Z,angle_Y):
-		angle_Z+=1
-		angle_Y+=0.05
-		self.scene.setHpr(self.scene,0,angle_Y,angle_Z) #p stands for pitch, H for heading, and r for roll
-		return 0
+		self.sun.reparentTo(self.render)
+		self.sun.setScale(1,1,1)
+		self.sun.setPos(self.bodyplace)
+		#now the lighting
+		#currently under work
+		
+		self.plight = PointLight('pointlight')
+		self.plight.setColorTemperature(700) #color defined by temperature, pretty usefull in this case
+		self.plight.setAttenuation((1,0,1)) #see https://www.panda3d.org/manual/?title=Lighting&oldid=4737 for more info
+		self.plnp = render.attachNewNode(self.plight)
+		self.plnp.setPos(0,0,0)
+		self.ast.setLight(self.plnp)
+		
+		#task manager stuff (further calculations will be here)
+		self.angle_Y=0
+		self.angle_Z=0
+		self.taskMgr.add(self.objectspin,"objectspintask")
+	
+	def showsimpletext(self,content,pos,scale): #shows a predefined, basic text on the screen (variable output only)
+		return OnscreenText(text=content,pos=pos,scale=scale)
+	
+	def temp_pos_update(self):
+		self.step+=0.01
+		self.bodyplace=(7*cos(self.step),7*sin(self.step),0)
+		self.ast.setPos(self.bodyplace)
+
+	def objectspin(self,task):
+		self.temp_pos_update()
+		try:
+			self.angle_Y+=0.01
+		except:
+			pass
+		render.setLight(self.plnp)
+		self.ast.setHpr(self.ast,0,0,0) #p stands for pitch, H for heading, and r for roll
+		return Task.cont  
 	
 	
 
