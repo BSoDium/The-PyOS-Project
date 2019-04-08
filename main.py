4333c65297feadfd7e270d6e59793f6c23f4c378
Filip Jerzy <keltapolis@gmail.com>
2019-04-08 22:43:09 +0200

    collision hitboxes work

--- main.py
+++ main.py
@@ -21,13 +21,15 @@ class world(ShowBase):
         self.pathname = os.path.dirname(sys.argv[0]) #currently unused
         # btw I found something about energy transmition through thermal radiation. I think it uses some Boltzmann formula stuff. Link here:
         # https://fr.wikibooks.org/wiki/Plan%C3%A9tologie/La_temp%C3%A9rature_de_surface_des_plan%C3%A8tes#Puissance_re%C3%A7ue_par_la_Terre
+        self.collision_solids=[] #collision related stuff - comments are useless - just RTFM
         self.light_Mngr=[]
         self.data=[[0,0,0,0,0.003,0,1,1,1,100000.00,True,[self.loader.loadModel(self.dir+"/Engine/lp_planet_0.egg"),self.loader.loadModel(self.dir+"/Engine/lp_planet_0.egg"),self.loader.loadModel(self.dir+"/Engine/lp_planet_2.egg"),self.loader.loadModel(self.dir+"/Engine/lp_planet_3.egg")],"lp_planet",False],
         [40,0,0,0,0.003,0,1,1,1,20.00,True,[self.loader.loadModel(self.dir+"/Engine/asteroid_1.egg"),self.loader.loadModel(self.dir+"/Engine/asteroid_2.egg")],"Ottilia",False],
         [0,70,10,0,0.005,0,3,3,3,40.00,True,[self.loader.loadModel(self.dir+"/Engine/asteroid_1.egg"),self.loader.loadModel(self.dir+"/Engine/asteroid_2.egg")],"Selena",False],[100,0,10,0,0,0,5,5,5,1000000,True,[self.loader.loadModel(self.dir+"/Engine/sun1.egg"),self.loader.loadModel(self.dir+"/Engine/sun2.egg")],"Sun",True]] 
-        # the correct reading syntax is [x,y,z,l,m,n,scale1,scale2,scale3,mass,static,[files],id,lightsource] for each body - x,y,z: position - l,m,n: speed - scale1,scale2,scale3: obvious (x,y,z) - mass: kg - static: boolean - [files]: panda3d readfiles list - id: str - lightsource: boolean -
-        
+        # the correct reading syntax is [x,y,z,l,m,n,scale1,scale2,scale3,mass,static,[files],id,lightsource,radius] for each body - x,y,z: position - l,m,n: speed - scale1,scale2,scale3: obvious (x,y,z) - mass: kg - static: boolean - [files]: panda3d readfiles list - id: str - lightsource: boolean - radius: positive value -
+        #if you want the hitbox to be correctly scaled, and your body to have reasonable proportions, your 3d model must be a 5*5 sphere, or at least have these proportions
         self.u_constant=6.67408*10**(-11) #just a quick reminder
+        self.u_radius=1 #just what I said earlier
         self.isphere=self.loader.loadModel(self.dir+"/Engine/InvertedSphere.egg") #loading skybox structure
         self.tex=loader.loadCubeMap(self.dir+'/Engine/cubemap_#.png')
 
@@ -35,11 +37,17 @@ class world(ShowBase):
         
         # see https://www.panda3d.org/manual/?title=Collision_Solids for further collision interaction informations
         base.graphicsEngine.openWindows()
-        for c in self.data:
-            for u in range(len(c[11])):
+        for c in self.data: # loading and displaying the preloaded planets and bodies
+            for u in range(len(c[11])): # loading each sub-file
                 c[11][u].reparentTo(self.render)
                 c[11][u].setScale(c[6],c[7],c[8])
                 c[11][u].setPos(c[0],c[1],c[2])
+                #setting the collision solid up
+            self.collision_solids.append([CollisionSphere(0,0,0,self.u_radius*(c[6]+c[7]+c[8])/3)]) #the radius is calculated by using the average scale * the u_radius 
+            self.collision_solids[len(self.collision_solids)-1].append(c[11][0].attachNewNode(CollisionNode(c[12])))
+            # the structure of the collision_solids list will be: [[1,2],[1,2],[1,2],...]
+            self.collision_solids[len(self.collision_solids)-1][1].node().addSolid(self.collision_solids[len(self.collision_solids)-1][0]) #I am definitely not explaining that
+            self.collision_solids[len(self.collision_solids)-1][1].show() # debugging purposes only
             print("placing body: done")
             if c[13]:
                 self.light_Mngr.append([PointLight(c[12]+"_other")])
