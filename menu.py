32a48132f5ecebc1d20af598fab17bba392a12af
Zudokakikuto <48180274+Zudokakikuto@users.noreply.github.com>
2019-06-11 01:41:33 +0200

    Added Menu in subfile and files in Engine
    +Menu

--- menu.py
+++ menu.py
@@ -4,11 +4,18 @@ from panda3d.core import *
 import os,ctypes
 from math import *
 from direct.filter.CommonFilters import CommonFilters
+from direct.gui.OnscreenText import OnscreenText 
+from direct.gui.OnscreenImage import OnscreenImage
+from direct.gui.DirectGui import *
+import sys
 
 user32 = ctypes.windll.user32
 user32.SetProcessDPIAware() #windows fullscreen compatibility, fixes the getsystemmetrics bug
+'''
 loadPrcFileData('', 'fullscreen true')
 loadPrcFileData('','win-size '+str(user32.GetSystemMetrics(0))+' '+str(user32.GetSystemMetrics(1)))
+'''
+
 loadPrcFileData('','framebuffer-multisample 1')
 loadPrcFileData('','multisamples 2')
 
@@ -18,9 +25,21 @@ class menu(ShowBase):
         ShowBase.__init__(self)
 
         # filters
+
         self.filters = CommonFilters(base.win, base.cam)
         render.setAntialias(AntialiasAttrib.MAuto)
 
+        # Rajout :
+
+        def quit():
+            sys.exit(0)
+        
+        maps_start=loader.loadModel(MAINDIR+'/Engine/start.egg')
+        maps_quit=loader.loadModel(MAINDIR+'/Engine/quit.egg')
+        self.start_button=DirectButton(pos=(0,0.35,0.60),frameColor=(0,0,0,0),scale=(0.4,0.4,0.1368),geom=(maps_start.find('**/Start'),maps_start.find('**/Start_push'),maps_start.find('**/Start_on'),maps_start.find('**/Start')))
+        self.quit_button=DirectButton(pos=(0,0.35,0.45),frameColor=(0,0,0,0),scale=(0.4,0.4,0.1368),geom=(maps_quit.find('**/Quit'),maps_quit.find('**/Quit_push'),maps_quit.find('**/Quit_on'),maps_quit.find('**/Quit')),command=quit)
+
+        # Fin du rajout :
 
         self.moon=self.loader.loadModel(MAINDIR+"/Engine/Icy.egg")
         self.moon.setScale(9,9,9)
@@ -30,6 +49,8 @@ class menu(ShowBase):
         self.intro_planet.setPos(0,0,0)
         self.intro_planet.reparentTo(render)
         self.intro_planet.setHpr(-110,0,0)
+        
+        #self.star_pic=OnscreenImage(image=MAINDIR+'/Engine/Stars.png',scale=(1))
         #camera
         self.cam.setPos(0,-70,0)
 
